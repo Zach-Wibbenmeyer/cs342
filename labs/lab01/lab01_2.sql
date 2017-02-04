a5f8d4838af29cc0eb108c40a43a75b8cffb40b1
Zach Wibbenmeyer <zdw3@calvin.edu>
2017-02-03 19:48:03 -0500

    Finished lab01

---
+++ labs/lab01/lab01_2.sql
@@ -0,0 +1,10 @@
+-- Figure out what rows are in the table
+DESCRIBE dual;
+-- There is only one row
+
+-- Figure out what values are in that row
+SELECT dummy FROM dual;
+-- There is only one item -> X
+
+-- Verify there is only one value in the table
+SELECT * FROM dual;
