3e3e3a482b6bf09bec088d834f1d8eca5b1c666a
Zach Wibbenmeyer <zdw3@calvin.edu>
2017-05-01 21:59:36 -0400

    Finished homework 11

--- 11post_relational/src/HelloWorld.java
+++ 11post_relational/src/HelloWorld.java
@@ -1,16 +1,45 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
 // The Java class will be hosted at the URI path "/helloworld"
-@Path("/hello")
+@Path("/")
 public class HelloWorld {
     // The Java method will process HTTP GET requests
     @GET
+    @Path("/hello")
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
     public String getClichedMessage() {
         // Return some cliched textual content
         return "Hello World";
+    }
+
+    @GET
+    @Path("/hello/api")
+    @Produces("text/plain")
+    public String getSimpleResponse() {
+        return "Getting...";
+    }
+
+    @PUT
+    @Path("/hello/api/{id}")
+    @Consumes("text/plain")
+    @Produces("text/plain")
+    public String putSimpleResponse(@PathParam("id") int id) {
+        return "Putting: " + id;
+    }
+
+    @POST
+    @Path("/hello/api/{str}")
+    @Produces("text/plain")
+    public String postSimpleResponse(@PathParam("str") String str) {
+        return "Posting: " + str;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes("text/plain")
+    @Produces("text/plain")
+    public String deleteSimple(@PathParam("id") int id) {
+        return "Deleting: " + id;
     }
 }
